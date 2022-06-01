6f92f73b8b6d1580777fe5090c9f9e6100ec469b
Sean Mackrory <mackrory@determined.ai>
2022-06-01 12:24:49 -0600

    dctx is working in Julia - remove previous test function

--- c.jl
+++ c.jl
@@ -1,2 +1,18 @@
-y = ccall((:hello, "libdctx"), Int32, (Cstring,), "World!")
-println(y)
+function dctx_open(rank, size, local_rank, local_size, cross_rank, cross_size, chief_addr, len)
+    ctx = Ref{Ptr{Cvoid}}()
+    ret = ccall((:dctx_open, "libdctx"), Int32,
+        (Ptr{Cvoid}, Int32, Int32, Int32, Int32, Int32, Int32, Cstring, Csize_t),
+        ctx, rank, size, local_rank, local_size, cross_rank, cross_size, chief_addr, len
+    )
+    if ret != 0
+        error("non-zero status returned")
+    end
+    return ctx
+end
+
+function dctx_close(ctx)
+    ccall((:dctx_close, "libdctx"), Cvoid, (Ptr{Cvoid},), ctx)
+end
+
+ctx = dctx_open(0, 2, 0, 2, 0, 1, "localhost:1234", 0)
+dctx_close(ctx)
--- dctx/dctx.c
+++ dctx/dctx.c
@@ -391,8 +391,3 @@ void unmarshal_free(struct dc_unmarshal *u){
     if(u->body) free(u->body);
     *u = (struct dc_unmarshal){0};
 }
-
-int hello(char *text) {
-  printf("Hello, %s\n", text);
-  return 7;
-}
