b314a9abc9cd137438d5e6d858da9d6e72faad39
Corey Staten <coreystaten@Coreys-MacBook-Pro.local>
2022-06-02 11:05:58 -0400

    Add script for docker build on agent.

---
+++ model/make-docker-launch.sh
@@ -0,0 +1,1 @@
+det command run --config-file launch-config.yml --context=. ./make-docker.sh
--- model/make-docker.sh
+++ model/make-docker.sh
@@ -1,1 +1,21 @@
-docker build -t determinedai/environments:julia-experimental -f Dockerfile .
+#!/bin/sh
+
+# Install docker
+apt-get update
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Build and push
+docker build -t mackrorysd/julia-harness:2 -f Dockerfile .
+docker login -u mackrorysd -p $(cat docker-token)
+docker push mackrorysd/julia-harness:2
