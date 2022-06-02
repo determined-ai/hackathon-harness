98ed37eb0f6076757d63aa07856f12b6fabf31b9
Ryan <rb@hpe.com>
2022-06-02 13:26:28 -0500

    make julia bindings more julia-y
    - raise errors in functions, rather than returning error codes
    - handle dc_result entirely inside gather_end

--- allgather.jl
+++ allgather.jl
@@ -1,51 +1,46 @@
-
 using Printf
 
 # int dctx_gather_start(struct dctx *dctx, char *data, size_t len);
 function dctx_gather_start(ctx, data)
-    @printf("gather_start\n");
-    status = @threadcall((:dctx_gather_start, "libdctx"), Cint,
+    status = ccall((:dctx_gather_start, "libdctx"), Cint,
         (Ptr{Cvoid}, Ptr{UInt8}, Csize_t),
         ctx, data, length(data))
-    @printf("gather_start done\n");
-    return status
+    if status != 0
+        error("dctx_gather_start failed")
+    end
 end
 
 # struct dc_result *dctx_gather_end(struct dctx *dctx);
 function dctx_gather_end(ctx)
-    @printf("gather_end\n");
-    result = @threadcall((:dctx_gather_end, "libdctx"), Ptr{Cvoid}, (Ptr{Cvoid},), ctx)
-    @printf("gather_end done\n");
-    return result
-end
+    dc_result = ccall((:dctx_gather_end, "libdctx"), Ptr{Cvoid}, (Ptr{Cvoid},), ctx)
 
-# bool dc_result_ok(struct dc_result *r);
-function dc_result_ok(dc_result)
-    ok = @threadcall((:dc_result_ok, "libdctx"), Cuchar, (Ptr{Cvoid},), dc_result)
-    return ok == 1
-end
+    # check status
+    ok = ccall((:dc_result_ok, "libdctx"), Cuchar, (Ptr{Cvoid},), dc_result)
+    if ok != 1
+        error("distributed result was not ok")
+    end
 
-# size_t dc_result_count(struct dc_result *r);
-function dc_result_count(dc_result)
-    count = @threadcall((:dc_result_count, "libdctx"), Csize_t, (Ptr{Cvoid},), dc_result)
-    return count
-end
+    count = ccall((:dc_result_count, "libdctx"), Csize_t, (Ptr{Cvoid},), dc_result)
 
-# char *dc_result_take(struct dc_result *r, size_t i, size_t *len);
-function dc_result_take(dc_result, i)
-    len = Ref(UInt64(0))
-    data = @threadcall((:dc_result_take, "libdctx"), Ptr{UInt8},
-                       (Ptr{Cvoid}, Csize_t, Ptr{Csize_t}),
-                       dc_result, i, len)
-    #  @printf("data[0]=%d\n", Int(unsafe_load(data)));
-    return unsafe_string(data, len[])
-    # return data, len[]
-end
+    # allocate vector output
+    out = Vector{String}(undef, count)
 
-# void dc_result_free(struct dc_result **r);
+    # Set each output
+    for i = 1:count
+        len = Ref(UInt64(0))
+        data = ccall((:dc_result_take, "libdctx"), Ptr{UInt8},
+                           (Ptr{Cvoid}, Csize_t, Ptr{Csize_t}),
+                           dc_result, i-1, len)
+        out[i] = unsafe_string(data, len[])
+    end
+
+    # TODO: dc_result_free
+
+    return out
+end
 
 function free(ptr)
-    #@threadcall(:free, Cvoid, (Ptr{Cvoid},), ptr)
+    #ccall(:free, Cvoid, (Ptr{Cvoid},), ptr)
 end
 
 function gather(ctx, data)
@@ -67,25 +62,25 @@ function gather(ctx, data)
         X = unsafe_wrap(Array{UInt8}, Ptr{UInt8}(bytes), len)
         buf = IOBuffer()
         write(buf, ser)
-        @threadcall(:free, Cvoid, (Ptr{Cvoid},), ser)
+        ccall(:free, Cvoid, (Ptr{Cvoid},), ser)
         push!(results, deserialize(buf))
     end
     return results
 end
 
 function dctx_open(rank, size, local_rank, local_size, cross_rank, cross_size, chief_host, chief_service)
-    ctx = @threadcall((:dctx_open2, "libdctx"), Ptr{Cvoid},
+    ctx = ccall((:dctx_open2, "libdctx"), Ptr{Cvoid},
         (Int32, Int32, Int32, Int32, Int32, Int32, Cstring, Cstring),
         rank, size, local_rank, local_size, cross_rank, cross_size, chief_host, chief_service
     )
-    # if ret != 0
-    #     error("non-zero status returned")
-    # end
+    if ctx == 0
+        error("dctx not returned")
+    end
     return ctx
 end
 
 function dctx_close(ctx)
-    @threadcall((:dctx_close2, "libdctx"), Cvoid, (Ptr{Cvoid},), ctx)
+    ccall((:dctx_close2, "libdctx"), Cvoid, (Ptr{Cvoid},), ctx)
 end
 
 chief   = dctx_open(0, 3, 0, 3, 0, 1, "localhost", "1234")
@@ -99,18 +94,8 @@ worker1_string = "worker1"
 worker2_string = "worker 2"
 
 dctx_gather_start(chief, chief_string)
-# @printf("%d\n", x)
-# if x != 0
-#     error("non-zero returned by dctx_gather_start on chief")
-# end
-
-if dctx_gather_start(worker1, worker1_string) != 0
-    error("non-zero returned by dctx_gather_start on worker1")
-end
-
-if dctx_gather_start(worker2, worker2_string) != 0
-    error("non-zero returned by dctx_gather_start on worker2")
-end
+dctx_gather_start(worker1, worker1_string)
+dctx_gather_start(worker2, worker2_string)
 
 @printf("sleeping\n");
 
@@ -122,54 +107,12 @@ c = dctx_gather_end(chief)
 w1 = dctx_gather_end(worker1)
 w2 = dctx_gather_end(worker2)
 
-if !dc_result_ok(c)
-    error("result not ok on chief")
-end
-
-if !dc_result_ok(w1)
-    error("result not ok on worker1")
-end
-
-if !dc_result_ok(w2)
-    error("result not ok on worker2")
-end
-
-@printf("counts\n");
-
-if dc_result_count(c) != 3
-    error("expected 3 result on chief")
-end
-
-if dc_result_count(w1) != 0
-    error("expected zero result on worker 1")
-end
-
-if dc_result_count(w2) != 0
-    error("expected zero result on worker 2")
-end
-
-@printf("takes\n");
-
-data = dc_result_take(c, 0)
-@printf("chief data: %s\n", data)
-if data != "chief"
-    error("Unexpected first result on chief")
-    free(data)
-end
-
-data = dc_result_take(c, 1)
-@printf("worker1 data: %s\n", data)
-if data != "worker1"
-    error("Unexpected second result on chief")
-    free(data)
-end
-
-data = dc_result_take(c, 2)
-@printf("worker2 data: %s\n", data)
-if data != "worker 2"
-    error("Unexpected third result on chief")
-    free(data)
-end
+print(c)
+print("\n")
+print(w1)
+print("\n")
+print(w2)
+print("\n")
 
 dctx_close(chief)
 dctx_close(worker1)
